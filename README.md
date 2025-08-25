7961fa5d3d8fbc1184e6bd84ccfe14ae0a68f9e5
alvarado0925 <alvarado0925@gmail.com>
2025-08-25 15:51:29 -0500

    Agregar manejo de RWMutex para acceso seguro al balance en concurrencia

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ go build --race sync/main.go
 
 # El WARNING de condición de carrera, solo me salió con run y no con build
 go run --race sync/main.go
+
+.\main.exe
+.\main
 ```
 
 Para usar GCC con Chocolatey, primero instala Chocolatey en tu sistema Windows, luego ejecuta el comando choco install mingw -y en PowerShell como administrador para instalar MinGW, que incluye el compilador GCC, y finalmente verifica la instalación con gcc -v.
--- sync/main.go
+++ sync/main.go
@@ -1,3 +1,14 @@
+// NOTE: Diferencias entre RWLock y no usar nada:
+
+// - Lock bloquea lecturas (con RLock) y escrituras (con Lock) de otras goroutines
+// - Unlock permite nuevas lecturas (con Rlock) y/o otra escritura (con Lock)
+// - RLock bloquea escrituras (Lock) pero no bloquea lecturas (RLock)
+// - RUnlock permite nuevas escrituras (y también lecturas, pero por la naturaleza de RLock,
+//   estas no se vieron bloqueadas nunca)
+//
+// En esencia, RLock de RWLock garantiza una secuencia de lecturas en donde el valor que lees
+// no se verá alterado por nuevos escritores, a diferencia de no usar nada.
+
 package main
 
 import (
@@ -5,33 +16,44 @@ import (
 	"sync"
 )
 
-var balance int = 100
-var mu sync.Mutex // Mutex para proteger el acceso a balance
+var balance int = 0
 
 // Deposit agrega una cantidad específica al balance de manera segura utilizando un mutex.
 // amount: La cantidad de dinero a depositar.
 // wg: Un puntero a un WaitGroup para sincronizar la finalización de la operación.
 // Esta función asegura que el acceso al balance sea seguro en un entorno concurrente,
 // bloqueando el mutex antes de modificar el balance y desbloqueándolo después.
-func Deposit(amount int, wg *sync.WaitGroup) {
+func Deposit(amount int, wg *sync.WaitGroup, mu *sync.RWMutex) {
 	defer wg.Done()
+	defer mu.Unlock() // Desbloquea el mutex después de modificar balance
+
 	mu.Lock() // Bloquea el mutex antes de modificar balance
 	balance += amount
-	mu.Unlock() // Desbloquea el mutex después de modificar balance
 }
 
-func Balance() int {
-	mu.Lock()         // Bloquea el mutex antes de leer balance
-	defer mu.Unlock() // Desbloquea el mutex después de leer balance
+func Balance(wg *sync.WaitGroup, mu *sync.RWMutex) int {
+	defer wg.Done()
+	defer mu.RUnlock() // Desbloquea el mutex después de leer balance
+
+	mu.RLock() // Bloquea el mutex para lectura
+	b := balance
+	fmt.Println("✅ Current balance is", b)
 	return balance
 }
 
+// 1 Deposit() -> Escribiendo (Posible condición de carrera)
+// N Balance() -> Muchos leyendo (Seguro)
 func main() {
 	var wg sync.WaitGroup
-	for i := 1; i <= 5; i++ {
+	var mu sync.RWMutex
+
+	for i := 1; i <= 20; i++ {
 		wg.Add(1)
-		go Deposit(i*100, &wg)
+		go Deposit(i, &wg, &mu)
+
+		wg.Add(1)
+		go Balance(&wg, &mu)
 	}
 	wg.Wait()
-	fmt.Println("Balance final:", Balance())
+	fmt.Println("Balance final:", balance)
 }
